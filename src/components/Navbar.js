b2ee3fe6e4ab2349b84675edc4389c43717975df
nirvanaGarciaV <84489764+nirvagarcia@users.noreply.github.com>
2024-03-12 19:40:14 -0500

    adding navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../styles/Navbar.css";
+import HomeIcon from '@mui/icons-material/Home';
+import { IconButton } from "@mui/material";
+
+const Navbar = () => {
+const links = [
+    { key: "skills", text: "Startup" },
+    { key: "projects", text: "Akira" },
+    { key: "contact", text: "Contáctanos" }
+];
+
+return (
+    <div className="navbar">
+        <div className="navbar-container">
+            <a href="#home" className="home-icon">
+                <IconButton aria-label="home" style={{padding:'0rem'}}>
+                <HomeIcon sx={{ color: 'black' }} />
+                </IconButton>
+            </a>
+
+            {links.map(link => (
+                <a key={link.key} href={`#${link.key}`} className="nav-link">
+                    {link.text}
+                </a>
+            ))}
+        </div>
+    </div>
+);
+}
+
+export default Navbar;
